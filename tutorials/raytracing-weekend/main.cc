aec5afecd218d5cd1f0989f8084baec8c2e9b203
holistudio <daniel.bin.lu@gmail.com>
2025-10-06 11:04:57 -0400

    Update main.cc
    Use vec3.h and color.h

--- tutorials/raytracing-weekend/main.cc
+++ tutorials/raytracing-weekend/main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -21,15 +24,19 @@ int main() {
         // pixels are written out in rows
         // every row of pixels is written out left to right
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
+            // auto r = double(i) / (image_width-1);
+            // auto g = double(j) / (image_height-1);
+            // auto b = 0.0;
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            // int ir = int(255.999 * r);
+            // int ig = int(255.999 * g);
+            // int ib = int(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+
+            // same result as above but with use of vec3 and color headers
+            auto pixel_color = color(double(i)/(image_width-1), double(j) / (image_height-1), 0.0);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
