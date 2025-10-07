db913150a8fee8b8c9f28e79dbb379dc6597f9d1
holistudio <daniel.bin.lu@gmail.com>
2025-10-07 12:30:13 -0400

    Update main.js
    Common add-ons

--- tutorials/learn-three-js/main.js
+++ tutorials/learn-three-js/main.js
@@ -1,1 +1,9 @@
 import * as THREE from 'three';
+
+// Addons do not need to be installed separately, 
+// but do need to be imported separately
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
