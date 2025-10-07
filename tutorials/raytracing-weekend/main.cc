3c2a2d266a041aaaebd5616b83b80c1d65a0c235
holistudio <daniel.bin.lu@gmail.com>
2025-10-06 21:04:34 -0400

    Update main.cc
    Debugged to match tutorial
     - pixel_delta_v and viewport_v must match!
     - always check _v and _u's are consistent!

--- tutorials/raytracing-weekend/main.cc
+++ tutorials/raytracing-weekend/main.cc
@@ -7,7 +7,7 @@
 color ray_color(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5, 0.7, 1.0);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.0, 0.0, 1.0);
 }
 
 int main() {
@@ -34,7 +34,7 @@ int main() {
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel
     auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_u / image_height;
+    auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
     auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
@@ -69,7 +69,7 @@ int main() {
             // auto pixel_color = color(double(i)/(image_width-1), double(j) / (image_height-1), 0.0);
             // write_color(std::cout, pixel_color);
 
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_u);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center; // not a unit vector
             ray r(camera_center, ray_direction);
 
